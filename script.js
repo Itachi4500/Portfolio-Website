348fd11e055a69ab45e128c08b9f4209e04f906c
Manoj More <manojmore0214@gmail.com>
2026-06-28 23:50:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -363,7 +363,7 @@
 const OWNER_EMAIL = 'manojmore0214@email.com';
 // Resume is hosted on GitHub — direct raw download URL
 const RESUME_FILE = 'https://github.com/Itachi4500/Portfolio-Website/tree/main/Resume';
-const RESUME_FILENAME = 'Manoj_Krishna_More_Resume.docx'; // filename shown to downloader
+const RESUME_FILENAME = 'Manoj.docx'; // filename shown to downloader
 
 function openResumeModal() {
   const modal = document.getElementById('resume-modal');
